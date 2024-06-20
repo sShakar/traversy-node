ac148c628fd7500ff5433869dae7e65ab42ef0f2
Shakar Kosar <shakarkosar01@gmail.com>
2024-06-20 16:57:16 +0300

    refactor

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const server = http.createServer((req, res) => {
   //   res.setHeader("Content-Type", "text/html");
   //   res.statusCode = 404;
 
-  res.writeHead(500, { "Content-Type": "application/json" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-  res.end(JSON.stringify({ message: "Server Error" }));
+  res.end("<h1>Hello World</h1>");
 });
 
 server.listen(PORT, () => {
