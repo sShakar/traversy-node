9dd610a26543a2ed3da1d0563d46984ffec206e4
sShakar <shakarkosar01@gmail.com>
2024-06-22 17:52:59 +0300

    added routes

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.statusCode = 404;
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed.");
+    }
+  } catch (error) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(error.message);
+  }
 });
 
 server.listen(PORT, () => {
