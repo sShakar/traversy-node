68fce05dfa626c60d2917399ef9f97210eb398e2
sShakar <shakarkosar01@gmail.com>
2024-06-22 15:15:45 +0300

    added env files

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import http from "http";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   //   res.setHeader("Content-Type", "text/html");
   //   res.statusCode = 404;
 
   res.writeHead(200, { "Content-Type": "text/html" });
-
   res.end("<h1>Hello World</h1>");
 });
 
