481e660e1899094628df7672e393e72408a0264e
Shakar Kosar <shakarkosar01@gmail.com>
2024-06-26 09:14:19 +0300

    read file

---
+++ fsDemo.js
@@ -0,0 +1,30 @@
+import fs from "fs/promises";
+
+// readFile() - callback
+
+// fs.readFile("./test.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// readFileSync() - Synchronous version
+
+// const data = fs.readFileSync("./test.txt", "utf8");
+// console.log(data);
+
+// readFile() - promise .then()
+// fs.readFile("./test.txt", "utf8")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// readFile() - async/await
+const readFile = async () => {
+  try {
+    const data = await fs.readFile("./test.txt", "utf8");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readFile();
