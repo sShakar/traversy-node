9c6b36facf453fe15777a28213af2ba9a26ac258
Shakar Kosar <shakarkosar01@gmail.com>
2024-06-26 09:20:41 +0300

    files

--- fsDemo.js
+++ fsDemo.js
@@ -1,19 +1,18 @@
+// import fs from 'fs';
 import fs from "fs/promises";
 
 // readFile() - callback
-
-// fs.readFile("./test.txt", "utf8", (err, data) => {
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
 //   if (err) throw err;
 //   console.log(data);
 // });
 
 // readFileSync() - Synchronous version
-
-// const data = fs.readFileSync("./test.txt", "utf8");
+// const data = fs.readFileSync('./test.txt', 'utf8');
 // console.log(data);
 
-// readFile() - promise .then()
-// fs.readFile("./test.txt", "utf8")
+// readFile() - Promise .then()
+// fs.readFile('./test.txt', 'utf8')
 //   .then((data) => console.log(data))
 //   .catch((err) => console.log(err));
 
@@ -27,4 +26,27 @@ const readFile = async () => {
   }
 };
 
+// writeFile()
+
+const writeFile = async () => {
+  try {
+    await fs.writeFile("./test.txt", "Hello, I am writing to this file");
+    console.log("File written to...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// appendFile()
+const appendFile = async () => {
+  try {
+    await fs.appendFile("./test.txt", "\nThis is appended text");
+    console.log("File appended to...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+writeFile();
+appendFile();
 readFile();
